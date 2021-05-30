87324a2d5075a88f17e692419079722a5f36b561
Rico Lang <rico.lang15@gmail.com>
2021-05-30 18:59:05 +0200

    Fix improper file serving handling on windows

--- web/file_handler.go
+++ web/file_handler.go
@@ -20,19 +20,12 @@ type SPAStaticHandler struct {
 // is suitable behavior for serving an SPA (single page application).
 func (h SPAStaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     // get the absolute path to prevent directory traversal
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-        // if we failed to get the absolute path respond with a 400 bad request
-        // and stop
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
+	path := r.URL.Path
     // prepend the path with the path to the Static directory
 	path = filepath.Join(h.StaticPath, path)
 
     // check whether a file exists at the given path
-	_, err = os.Stat(path)
+	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		// file does not exist, serve index.html
 		http.ServeFile(w, r, filepath.Join(h.StaticPath, h.IndexPath))
